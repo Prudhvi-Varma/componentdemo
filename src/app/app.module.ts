afc383db11d32615b6c67ff5137993fd0244535a
prudhvivarma123 <103100669+prudhvivarma123@users.noreply.github.com>
2022-05-03 11:11:39 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
